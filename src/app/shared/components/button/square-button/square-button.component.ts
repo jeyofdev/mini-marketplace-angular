abb262f118407860b2afa513dd064b22db33aeed
jeyofdev <jgregoire.pro@gmail.com>
2023-07-31 16:10:26 +0200

    feat : create custom Button component

---
+++ src/app/shared/components/button/square-button/square-button.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { IconDefinition } from '@fortawesome/free-brands-svg-icons';
+
+@Component({
+	selector: 'app-square-button',
+	templateUrl: './square-button.component.html',
+	styleUrls: ['./square-button.component.scss'],
+})
+export class SquareButtonComponent {
+	@Input() label!: string;
+	@Input() color!: string;
+	@Input() icon!: IconDefinition;
+	@Input() size!: string | number;
+	@Input() outline!: boolean;
+
+	getWidth(): string {
+		return this.size + 'px';
+	}
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SquareButtonComponent } from './components/button/square-button/square-button.component';
 
 @NgModule({
-	declarations: [],
+	declarations: [SquareButtonComponent],
 	imports: [CommonModule, MaterialModule, FontAwesomeModule],
-	exports: [MaterialModule, FontAwesomeModule],
+	exports: [MaterialModule, FontAwesomeModule, SquareButtonComponent],
 })
 export class SharedModule {}
