77e9e849de66433c3db6c3691bf04ef6ec8f64cd
jeyofdev <jgregoire.pro@gmail.com>
2023-07-31 17:24:54 +0200

    feat : create custom TextField component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SquareButtonComponent } from './components/button/square-button/square-button.component';
+import { TextFieldComponent } from './components/input/text-field/text-field.component';
 
 @NgModule({
-	declarations: [SquareButtonComponent],
+	declarations: [SquareButtonComponent, TextFieldComponent],
 	imports: [CommonModule, MaterialModule, FontAwesomeModule],
-	exports: [MaterialModule, FontAwesomeModule, SquareButtonComponent],
+	exports: [
+		MaterialModule,
+		FontAwesomeModule,
+		SquareButtonComponent,
+		TextFieldComponent,
+	],
 })
 export class SharedModule {}
