424143e6dae6046bbc84ad3822122b33618683a3
Jérémy Grégoire <46073105+jeyofdev@users.noreply.github.com>
2023-08-02 14:52:34 +0200

    feat : init reactive form for pages dashboard/auth (#5)
    * feat : init reactive form for page dashboard/auth/register
    
    * feat : init reactive form for page dashboard/auth/login
    
    * feat : init validators for auth forms

--- src/app/dashboard/auth/components/register/register.component.ts
+++ src/app/dashboard/auth/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { faGithub, faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { IImage } from 'src/app/shared/model/image.model';
 import { ISocialProvider } from 'src/app/shared/model/social-provider.model';
@@ -12,6 +13,11 @@ export class RegisterComponent implements OnInit {
 	socialProviders!: ISocialProvider[];
 	image!: IImage;
 	hidePassword!: boolean;
+	mainForm!: FormGroup;
+	personnalInfosForm!: FormGroup;
+	passwordForm!: FormGroup;
+
+	constructor(private formBuilder: FormBuilder) {}
 
 	ngOnInit(): void {
 		this.hidePassword = false;
@@ -37,5 +43,34 @@ export class RegisterComponent implements OnInit {
 			alt: '',
 			position: 'top',
 		};
+
+		this.initFormControls();
+		this.initRegistrationForm();
+	}
+
+	onMainFormSubmit(): void {
+		// eslint-disable-next-line no-console
+		console.log(this.mainForm.value);
+	}
+
+	private initRegistrationForm(): void {
+		this.mainForm = this.formBuilder.group({
+			personnalInfos: this.personnalInfosForm,
+			email: ['', Validators.required],
+			password: this.passwordForm,
+		});
+	}
+
+	private initFormControls(): void {
+		this.personnalInfosForm = this.formBuilder.group({
+			firstname: ['', Validators.required],
+			lastname: ['', Validators.required],
+			username: ['', [Validators.required, Validators.minLength(5)]],
+		});
+
+		this.passwordForm = this.formBuilder.group({
+			password: ['', Validators.required],
+			confirmPassword: ['', Validators.required],
+		});
 	}
 }
--- src/app/shared/components/input/text-field/text-field.component.ts
+++ src/app/shared/components/input/text-field/text-field.component.ts
@@ -1,17 +1,41 @@
-import { Component, Input, OnInit } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, Input, OnInit, forwardRef } from '@angular/core';
+import {
+	ControlValueAccessor,
+	FormControl,
+	FormGroup,
+	NG_VALUE_ACCESSOR,
+} from '@angular/forms';
 
 @Component({
 	selector: 'app-text-field',
 	templateUrl: './text-field.component.html',
 	styleUrls: ['./text-field.component.scss'],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => TextFieldComponent),
+			multi: true,
+		},
+	],
 })
-export class TextFieldComponent implements OnInit {
+export class TextFieldComponent implements OnInit, ControlValueAccessor {
 	@Input() type!: 'text' | 'password';
 	@Input() appearance!: 'outline' | 'fill';
 	@Input() name!: string;
 	@Input() label!: string;
 	@Input() hidePassword!: boolean;
 	@Input() endIcon!: boolean;
+	@Input() parentForm!: FormGroup;
+
+	@Input() control!: string[]; // [controlGroup, controlField]
+	@Input() groupName!: string;
+
+	isDisabled!: boolean;
+	value!: string;
+
+	changed!: (value: string) => void;
+	onTouched!: () => void;
 
 	finalType!: 'text' | 'password';
 
@@ -22,5 +46,36 @@ export class TextFieldComponent implements OnInit {
 
 	shownPassword(): void {
 		this.hidePassword = !this.hidePassword;
+	}
+
+	writeValue(value: string): void {
+		this.value = value;
+	}
+
+	onChange(event: Event): void {
+		const value: string = (<HTMLInputElement>event.target).value;
+		this.changed(value);
+	}
+
+	registerOnChange(fn: any): void {
+		this.changed = fn;
+	}
+
+	registerOnTouched(fn: any): void {
+		this.onTouched = fn;
+	}
+
+	get formControl() {
+		return this.getFormControl();
+	}
+
+	private getFormControl(): FormControl {
+		if (this.groupName) {
+			const group = this.groupName.slice(0, this.groupName.length - 4);
+			const control = this.parentForm.controls[group].get(this.name);
+			return control as FormControl;
+		} else {
+			return this.parentForm.get(this.name) as FormControl;
+		}
 	}
 }
