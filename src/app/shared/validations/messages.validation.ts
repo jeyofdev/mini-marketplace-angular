1b28e401785d7ad6719491064c652c00c3453292
Jérémy Grégoire <46073105+jeyofdev@users.noreply.github.com>
2023-08-07 11:40:01 +0200

    feat : validation modal add product (#15)

--- src/app/shared/validations/messages.validation.ts
+++ src/app/shared/validations/messages.validation.ts
@@ -7,7 +7,44 @@ export const addCategoryValidationMessages = {
 		},
 		maxlength: {
 			message: 'This field must contain a maximum of 30 characters',
-			value: 20,
+			value: 30,
+		},
+	},
+};
+
+export const addProductValidationMessages = {
+	brandName: {
+		required: { message: 'This field is required' },
+		minlength: {
+			message: 'This field must contain at least 3 characters',
+			value: 3,
+		},
+		maxlength: {
+			message: 'This field must contain a maximum of 30 characters',
+			value: 30,
+		},
+	},
+	modelName: {
+		required: { message: 'This field is required' },
+		minlength: {
+			message: 'This field must contain at least 3 characters',
+			value: 3,
+		},
+		maxlength: {
+			message: 'This field must contain a maximum of 30 characters',
+			value: 30,
+		},
+	},
+	category: {
+		required: { message: 'A category must be selected' },
+	},
+	sizes: {
+		required: { message: 'A size must be selected' },
+	},
+	price: {
+		required: { message: 'This field is required' },
+		pattern: {
+			message: 'the price must be in the format 10 or 10.5 or 10.99 etc...',
 		},
 	},
 };
