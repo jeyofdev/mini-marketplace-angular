cc377190e78c794c24a9092ac19de24716e4b662
jeyofdev <jgregoire.pro@gmail.com>
2023-08-08 11:59:20 +0200

    migration : component alert with primeng

--- src/app/shared/primeng.module.ts
+++ src/app/shared/primeng.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { MessageModule } from 'primeng/message';
+import { MessagesModule } from 'primeng/messages';
 
 @NgModule({
-	exports: [ButtonModule, InputTextModule],
+	exports: [ButtonModule, InputTextModule, MessageModule, MessagesModule],
 })
 export class PrimengModule {}
