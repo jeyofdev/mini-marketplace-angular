f7ddc64118312ef13620cc03264130e25ab4b626
jeyofdev <jgregoire.pro@gmail.com>
2023-08-19 17:20:41 +0200

    feat : init layout in pages web

--- src/app/web/web.module.ts
+++ src/app/web/web.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { WebRoutingModule } from './web-routing.module';
 import { HomeComponent } from './pages/home/home.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
-	declarations: [HomeComponent],
+	declarations: [HomeComponent, LayoutComponent],
 	imports: [CommonModule, WebRoutingModule],
 })
 export class WebModule {}
