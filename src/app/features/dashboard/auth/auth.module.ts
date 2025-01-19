25a319bbe8365935c782e756185991e583e339fb
jeyofdev <jgregoire.dev@gmail.com>
2025-01-19 19:20:12 +0100

    feat : add component form-register

--- src/app/features/dashboard/auth/auth.module.ts
+++ src/app/features/dashboard/auth/auth.module.ts
@@ -7,9 +7,15 @@ import { SharedModule } from '@shared/shared.module';
 import { RegisterComponent } from '@dashboard/auth/pages/register/register.component';
 import { DashboardModule } from '@dashboard/dashboard.module';
 import { FormLoginComponent } from './components/form/form-login/form-login.component';
+import { FormRegisterComponent } from './components/form/form-register/form-register.component';
 
 @NgModule({
-	declarations: [LoginComponent, RegisterComponent, FormLoginComponent],
+	declarations: [
+		LoginComponent,
+		RegisterComponent,
+		FormLoginComponent,
+		FormRegisterComponent,
+	],
 	imports: [CommonModule, AuthRoutingModule, SharedModule, DashboardModule],
 })
 export class AuthModule {}
